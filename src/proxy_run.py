79e071ab67991abf599ece500fa61b6401b152db
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2021-11-21 14:16:14 +0000

    Started periodical save

--- src/proxy_run.py
+++ src/proxy_run.py
@@ -1,5 +1,14 @@
 from server import Proxy
+from zmq.eventloop.ioloop import PeriodicCallback
+import _pickle as cPickle
+
+def save_pickle(proxy):
+    print("Saving...")
+    with open(r"storage.pickle", "wb") as output_file:
+        cPickle.dump(proxy, output_file)
 
 if __name__ == '__main__':
     proxy = Proxy()
+    scheduler = PeriodicCallback(save_pickle(proxy), 5)
+    scheduler.start()
     proxy.start()
