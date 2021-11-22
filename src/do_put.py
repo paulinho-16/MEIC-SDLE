bdc1948e1741ec0e3f2c0014f5d2d98b2be0e757
Telmooo <telmoabaptista@hotmail.com>
2021-11-22 20:49:42 +0000

    Created makefile and updated do_sub and do_put

--- src/do_put.py
+++ src/do_put.py
@@ -1,5 +1,14 @@
 from xmlrpc.client import ServerProxy
+import sys
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
+    if len(sys.argv) < 3:
+        print(f"Usage:\n {sys.argv[0]} [topic] [message]")
+        sys.exit(1)
+    
+    topic = sys.argv[1]
+    message = " ".join(sys.argv[2:])
+
     s = ServerProxy("http://127.0.0.1:8080")
-    s.put("A","mensagem")
+
+    s.put(topic, message)
--- src/do_sub.py
+++ src/do_sub.py
@@ -17,20 +17,16 @@ if __name__ == '__main__':
     s = ServerProxy("http://127.0.0.1:8081")
     
     if len(sys.argv) != 3:
-        print("Usage:\n get [NTimes]\n subscribe [Topic]\n unsubscribe [Topic]")
+        print(f"Usage:\n \t{sys.argv[0]} get [nTimes]\n\t{sys.argv[0]} sub [topic]\n\t{sys.argv[0]} unsub [topic]")
 
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print(f"Wrong number of arguments\nUsage: python {sys.argv[0]} <topic>\n get [num]\n unsubscribe <topic>")
-
-    num = None
-    topic = ""
-
-    if sys.argv[1] == "subscribe":
+    if sys.argv[1] == "sub":
         s.subscribe(sys.argv[2])
     elif sys.argv[1] == "get":
-        dogets(int(sys.argv[2]))
-    elif sys.argv[1] == "unsubscribe":
+        try:
+            dogets(int(sys.argv[2]))
+        except ValueError:
+            print("Argument [nTimes] must be an integer")
+    elif sys.argv[1] == "unsub":
         s.unsubscribe(sys.argv[2].encode('utf-8'))
     else:
         s.crash()
-    
